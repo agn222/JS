f6f5963f55e1e0179cd0676c0ab56cb0806e5d5a
agn222 <naikatharva2504@gmail.com>
2025-08-22 19:13:03 +0000

    conversion

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,16 +1,16 @@
 let score = 33 // this is a number
 
-console.log(typeof score)
+// console.log(typeof score)
 // console.log(typeof(score))
 
 let stringscore = "33a" //this is a string
 
-console.log(typeof stringscore)
+// console.log(typeof stringscore)
 // console.log(typeof(stringscore))
 
 let valueInString = Number(stringscore)// converting a string into number
-console.log(typeof valueInString)
-console.log(valueInString) // not a number (33a)
+// console.log(typeof valueInString)
+// console.log(valueInString) // not a number (33a)
 
 //converting
 // "33" => 33
@@ -19,7 +19,7 @@ console.log(valueInString) // not a number (33a)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 //converting following into boolean
 
 // "" => false
@@ -28,5 +28,26 @@ console.log(booleanIsLoggedIn)
 let someNumbr = 66
 let stringNumber = String(someNumbr)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// **********Operations*********
+value = 69
+let negative =-value
+console.log(negative) 
+
+
+let  str1 = "hello"
+let str2 = " javascript"
+
+let str3 = str1 + str2;
+// console.log(str3)
+
+// console.log( "2" + 6)// string first  26
+// console.log(2 + "6" ) // 2 is no 6 is string hence not sdded in 2
+// console.log(2 + 6 +"6") // number first 86
+// console.log( "2" + 6 + 6)// string first hence 6 is not added to the last digit 2 ans is 266
+
+
+console.log(+true) //1
+
