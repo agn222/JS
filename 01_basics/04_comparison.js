cc35528544e7f6c89ff14cb32c8e5858da264058
agn222 <naikatharva2504@gmail.com>
2025-08-22 19:40:56 +0000

    summary

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log("2" > 1)
+// console.log("02" > 1)// automatically converts 2 into number
+
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0) //comparison convrts null into nos 
+
+console.log(undefined > 0)
+console.log(undefined >= 0)
+console.log(undefined == 0)
+
+
+// strict check ===
+
+
+console.log("2" ===2)
---
+++ 01_basics/data_typesSummary.js
@@ -0,0 +1,7 @@
+// Primitive
+// 7 types
+// string, number, boolean, null, undefined, Symbol, bigint
+
+
+// Refrence Types or Non Primitive
+// Array, Objects, Functions 
