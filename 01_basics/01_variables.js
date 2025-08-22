ae13660fd1bed3468d58ec52cb83982c4560f181
agn222 <naikatharva2504@gmail.com>
2025-08-22 17:32:22 +0000

    push

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accounId = 2042005
+let accountName = "Atharva"
+var accountCity = "Mumbai"
+accountreg = "231010040"
+let accountState ;
+//  accountId = 231010040  not allowed wont work
+
+console.table([accounId,accountCity,accountName,accountreg, accountState])
+// Note : const cannot be changed onced declares
+// let var can be changed
+// Prefer using let to declare variables
+
