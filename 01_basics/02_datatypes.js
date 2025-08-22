d3fef99e3f95a1e80b0f08116afc8c6cbb20e7d9
agn222 <naikatharva2504@gmail.com>
2025-08-22 18:14:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" ; //treats all JS code as newer version
+
+// alert( 3*3)// we are using nodejs not browser
+
+console.log(3
+    +3); //code readablity must be high
+// documentation tc39, mdn
+
+let name = "atharva"
+let age = 20
+let isLoggedIn = true
+//Primitive datatypes
+// Number => Range 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/falsr
+// null => standalone value
+// undefined => 
+// symbol => used in react n uniqueness
+
+//object 
+
+console.log(typeof null) // type is object
+console.log(typeof undefined) //type is undfined
+
