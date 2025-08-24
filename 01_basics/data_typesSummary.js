8c9d418ec581d44471f5adf9056cec01bf894ca9
agn222 <naikatharva2504@gmail.com>
2025-08-24 04:39:10 +0000

    stack and heap

--- 01_basics/data_typesSummary.js
+++ 01_basics/data_typesSummary.js
@@ -2,6 +2,33 @@
 // 7 types
 // string, number, boolean, null, undefined, Symbol, bigint
 
+// const { useLayoutEffect } = require("react");
+
 
 // Refrence Types or Non Primitive
 // Array, Objects, Functions 
+
+
+//********************Stack & Heap************************ 
+
+// let atharva = "gian";
+
+// atharva = "nobita"
+
+// console.log(atharva);
+
+let athu = {
+    nav :"gian",
+    age : "20",
+    height : "180"
+
+
+}
+let amit = athu;
+athu.nav = "nobita"
+
+console.log(athu.nav);
+console.log(amit.nav)
+
+
+
