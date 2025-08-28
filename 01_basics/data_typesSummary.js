fed20793273a5609a8d2d867e0b37094a2db1e9d
agn222 <naikatharva2504@gmail.com>
2025-08-28 14:53:39 +0000

    array1

--- 01_basics/data_typesSummary.js
+++ 01_basics/data_typesSummary.js
@@ -11,12 +11,13 @@
 
 //********************Stack & Heap************************ 
 
-// let atharva = "gian";
+let atharva = "gian"; // here primitive datatype string is used hence data is stored in stack 
+// stack 
+niv = atharva
+atharva = "nobita" // here i made changes in atharva, but niv did not change as in stack, whenever the data is accessed a copy is made and no changes happen to ooriginal data
 
-// atharva = "nobita"
-
-// console.log(atharva);
-
+console.log(atharva);
+console.log(niv);
 let athu = {
     nav :"gian",
     age : "20",
@@ -29,6 +30,4 @@ athu.nav = "nobita"
 
 console.log(athu.nav);
 console.log(amit.nav)
-
-
-
+// whereas in heap where non primitve data types like arrays are stored, no copy is madr and when changes occur the original data will be also modified
--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,3 +2,43 @@
 
 const myArray = [0 , 1 , 2, 3]
 console.log(myArray[0])
+//shallow copy share same refrence 
+//deep copy does not share same refrence
+
+//Array Methods
+
+myArray.push(6) // 6 is added as the n-1 th element
+console.log(myArray)
+
+myArray.pop() // removes last element from arrray
+console.log(myArray)
+
+myArray.unshift(9) // 9 is inserted as first element
+console.log(myArray)
+
+myArray.shift() // removes first element from array
+
+console.log(myArray)
+
+console.log(myArray.includes(1)) // checks if 1 is there in array
+
+console.log(myArray.indexOf(2)) // tell me index of element 2
+
+const newArray = myArray.join() // converts array into string
+console.log(newArray)
+
+
+// slice
+console.log("A" ,myArray)
+
+const myn1 = myArray.slice(1,3)
+console.log(myn1)
+console.log("B", myArray)
+// splice
+console.log("A" ,myArray)
+
+const myn2 = myArray.splice(1,3)
+console.log(myn2)
+console.log("B", myArray)
+// splice modifies original array and includes "to" range
+// slice doesnot modify originak array and does not includes the "to" range
