86ddaa29afc6f66790dc3ca6cfd6764f3d09180f
agn222 <naikatharva2504@gmail.com>
2025-08-29 19:32:56 +0000

    scopes1

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,18 @@
+
+// {} this is scope
+
+if (true){
+    let a = 10
+const b =20
+var c = 30
+
+}
+
+
+
+// console.log(a);
+// console.log(b)
+console.log(c) // var c is accessible outside scope
+// whereas let and const are not accessible outside scope{}
+
+
