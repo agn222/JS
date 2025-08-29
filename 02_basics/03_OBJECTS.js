fcd57e0edf225f2a7c6d47d7e5d179841e7b46ad
agn222 <naikatharva2504@gmail.com>
2025-08-29 11:03:51 +0000

    object

---
+++ 02_basics/03_OBJECTS.js
@@ -0,0 +1,41 @@
+
+
+//singleton
+Object.create
+
+
+//object literals
+
+const mySym = Symbol("Key1")
+
+const JsUser = {
+    name : "Atharva",
+    "full name" : "Atharva gajanan naik",
+    age : 20,
+    [mySym] : "mykey",
+    location : "Mumbai",
+    email : "atharvagnaik@gmail.com",
+    isLogginIN : true,
+    lastLoginDays : ["Monday", "Tuesday"]
+    }
+// console.log(JsUser.email) // prints email
+// console.log(JsUser["email"])  // prints email
+// console.log(JsUser[mySym]) // prints symbol mykey
+
+JsUser.email = "atharvagnaik@google.com"
+// console.log(JsUser) // now email changes 
+Object.freeze(JsUser)
+
+JsUser.email = "atharvagnaik@gpt.com" // email would not be modified
+// console.log(JsUser)
+
+JsUser.greeting = function()
+{
+    console.log("HELLO JS USER");
+}
+JsUser.greetingTwo = function()
+{
+    console.log(`HELLO JS USER ,${this.name}`)}
+
+JsUser.greeting();
+JsUser.greetingTwo();
