9c6e6efcff0f588948218bf6a85fd32639046e2c
agn222 <naikatharva2504@gmail.com>
2025-08-29 18:18:12 +0000

    object2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,8 +1,8 @@
-// const tinderUser ={}
+const tinderUser ={}
 
-// tinderUser.id = "123abc"
-// tinderUser.name = "Atharva"
-// tinderUser.isLoggedIn = false
+tinderUser.id = "123abc"
+tinderUser.name = "Atharva"
+tinderUser.isLoggedIn = false
 
 // console.log(tinderUser)
 
@@ -20,5 +20,21 @@ fullname : {
 
 // console.log(regularUser.fullname.userfullname)
 
-const obj1 = { 1: "a"}
+const obj1 = { 1: "a", 2 : "b"}
+const obj2 = { 3: "a", 4 : "b"}
+const obj4 = { 5: "a", 6 : "b"}
 
+// const obj3 = Object.assign({}, obj1,obj2,obj4) //merges ob1, obj2, obj4
+
+// console.log(obj3);
+
+const obj3 = {...obj1, ...obj2,...obj4}
+console.log(obj3)
+console.log(tinderUser);
+console.log(Object.keys(tinderUser))  // gives me keys ( variables/constant)
+console.log(Object.values(tinderUser)) // gives me values of keys like values of variables
+console.log(Object.entries(tinderUser))// give me keys and corresponding value in an array
+
+console.log(tinderUser.hasOwnProperty('name'))
+
+ 
