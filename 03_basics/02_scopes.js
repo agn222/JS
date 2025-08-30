209dd43cce7c6db6d3e4602bc2c94197aa3321a7
agn222 <naikatharva2504@gmail.com>
2025-08-30 18:35:13 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,6 +1,8 @@
 
 // {} this is scope
 
+// const { use } = require("react")
+
 if (true){
     let a = 10
 const b =20
@@ -12,7 +14,7 @@ var c = 30
 
 // console.log(a);
 // console.log(b)
-console.log(c) // var c is accessible outside scope
+// console.log(c) // var c is accessible outside scope
 // whereas let and const are not accessible outside scope{}
 
 function one (){
@@ -20,7 +22,7 @@ function one (){
     function two()
     {
         const website = "youtube"
-        console.log(username);
+        // console.log(username);
 
     }  
 //   console.log(website) // wrbsite is not accessible as it is out of scope
@@ -28,3 +30,30 @@ two()
 }
 one()
 
+
+if(true){
+    const username= "atharva"
+    if(username==="atharva"){
+        const website = "youtube"
+        // console.log(username + website)
+
+    }
+    // console.log(website)
+}
+
+// console.log(username)
+
+
+// *******************************interesting*********************
+
+function AddOne(num)
+{
+    return num+1
+}
+console.log (AddOne(5))
+
+const addTwo = function(num){
+    return num+2
+}
+
+console.log(addTwo(1))
